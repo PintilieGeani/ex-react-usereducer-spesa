7e0da2885ad19894a96a7944d45f2a62f305e6ca
PintilieGeani <pintilie.geany@yahoo.it>
2025-08-27 17:16:14 +0200

    feat: seconda Milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 function App() {
   const products = [
@@ -7,17 +8,44 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([])
+
+  const handleAdd = (p) => {
+      const presente = addedProducts.some((e) =>e.Product === p.name )
+      if(!presente){
+        setAddedProducts([...addedProducts, {Product: p.name, Quantity: 1, Prezzo: p.price}])
+      }else{console.log("Prodotto già nel carrello")}
+  }
 
 
   return (
     <>
-    <div>
-      {products.map((p) => (
-        <div className="card">
-          <h4>Nome: {p.name}</h4>
-          <p><strong>Prezzo:</strong>{p.price} €</p>
-        </div>
-      ))}
+    <div className="inpaginazione">
+      <div className="prodotti">
+        {products.map((p, i) => (
+          <div key={i} className="card">
+            <div className="card-text">
+              <h4>Nome: {p.name}</h4>
+              <p><strong>Prezzo:</strong>{p.price} €</p>
+            </div>
+            <div className="card-buttons">
+              <button onClick={() => handleAdd(p)} className="btn bg-green">Aggiungi al carrello</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="carrello">
+        <p>Carrello</p>
+        <ul>
+        {addedProducts.map((p, i) => (
+          <li key={i}>
+            <p>Prodotto: {p.Product}</p>
+            <p>Quantità: {p.Quantity}</p>
+            <p>Prezzo: {p.Prezzo}</p>
+          </li>
+        ))}
+        </ul>
+      </div>
     </div>
     </>
   )
